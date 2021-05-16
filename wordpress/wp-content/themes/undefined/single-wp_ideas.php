40a0bcf98d439573c2f232394c5389fab5eda1e4
Lucas Pereira Ramos <lramos_ads@hotmail.com>
2021-05-16 15:31:54 -0300

    Correção

--- wordpress/wp-content/themes/undefined/single-wp_ideas.php
+++ wordpress/wp-content/themes/undefined/single-wp_ideas.php
@@ -22,7 +22,7 @@
           </span>
         </p>
       </div>
-      <?php if (get_field('idea_post__download', get_the_ID()) || true) : ?>
+      <?php if (get_field('idea_post__download', get_the_ID())) : ?>
         <div class="download">
           <p>
             <a href="<?= get_field('idea_post__download', $post_id); ?>" target="_blank">
